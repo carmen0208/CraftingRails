fdc3c8b3868641d06c69c4d31f5f33e4a22f19b1
carmen0208 <13770576@qq.com>
2016-08-31 13:30:59 +0800

    ActiveModel of how to add attribute_prefix

--- mail_form/test/mail_form_test.rb
+++ mail_form/test/mail_form_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
+require 'fixtures/sample_mail'
 
 class MailFormTest < ActiveSupport::TestCase
   test "truth" do
     assert_kind_of Module, MailForm
+  end
+  test "sample mail has name nad email as attributes" do
+    sample = SampleMail.new
+    sample.name = "Carmen"
+    assert_equal "Carmen", sample.name
+    sample.email = "carmen.liu@activenetwork.com"
+    assert_equal "carmen.liu@activenetwork.com", sample.email
+
+    sample.clear_name
+    sample.clear_email
+    assert_nil sample.name
+    assert_nil sample.email
   end
 end
