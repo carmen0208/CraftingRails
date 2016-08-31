c98d50968f92d4bfa8d6cb304866d1a9b55c3a43
carmen0208 <13770576@qq.com>
2016-08-31 09:59:10 +0800

    future function of new renderer

--- pdf_renderer/test/integration/pdf_delivery_test.rb
+++ pdf_renderer/test/integration/pdf_delivery_test.rb
@@ -9,7 +9,31 @@ class PdfDeliverTest < ActionDispatch::IntegrationTest
 
     assert_equal "attachment; filename=\"contents.pdf\"",
       headers["Content-Disposition"]
-#rails/actionpack/lib/action_dispatch/http/mime_types.rb <- the reason why rails know which content type to set in our response?
+    #rails/actionpack/lib/action_dispatch/http/mime_types.rb <- the reason why rails know which content type to set in our response?
+    ## http://www.ietf.org/rfc/rfc4627.txt
+    # http://www.json.org/JSONRequest.html
+    #Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest )
+    #Mime::Type.register "application/pdf", :pdf, [], %w(pdf)
+    #Mime::Type.register "application/zip", :zip, [], %w(zip)
+
+    assert_equal "application/pdf", headers["Content-Type"]
+  end
+
+  test "pdf request sends a pdf as file and use specific template" do
+    get another_path(format: :pdf)
+
+    assert_match "PDF", response.body
+    assert_equal "binary", headers["Content-Transfer-Encoding"]
+
+    assert_equal "attachment; filename=\"contents.pdf\"",
+      headers["Content-Disposition"]
+    #rails/actionpack/lib/action_dispatch/http/mime_types.rb <- the reason why rails know which content type to set in our response?
+    ## http://www.ietf.org/rfc/rfc4627.txt
+    # http://www.json.org/JSONRequest.html
+    #Mime::Type.register "application/json", :json, %w( text/x-json application/jsonrequest )
+    #Mime::Type.register "application/pdf", :pdf, [], %w(pdf)
+    #Mime::Type.register "application/zip", :zip, [], %w(zip)
+
     assert_equal "application/pdf", headers["Content-Type"]
   end
 end
