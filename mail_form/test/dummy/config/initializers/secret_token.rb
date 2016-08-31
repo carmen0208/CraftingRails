59a08c3600da667c2442e036bc703bc90d4764d7
carmen0208 <13770576@qq.com>
2016-08-31 10:26:57 +0800

    Building Model with ActiveModel: inital project by 'rails plugin new mail_form​'

---
+++ mail_form/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'b79f7468d70d3582aa0d7edb09ef9f5ea79559a9614edcbe09094da65154f8d652c98d5c7f929da44ec9c54a98fc9d45f044ff558e22032da500bc8ea61f560d'
