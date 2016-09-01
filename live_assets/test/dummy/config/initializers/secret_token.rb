8f95d22ba0fa44b94ecd55554e1c31a357c431b9
carmen0208 <13770576@qq.com>
2016-09-01 20:49:46 +0800

    start project : Live Streaming

---
+++ live_assets/test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '0c44ab5fbd6909201da8faa8e7d13537cc4b8f1d7651073cba6534df259f6385e8340373293dceb369ae057fa074284afe2413ef8421e3d23d9346ea5c4aa465'
